d7470341edf5e6cb722aaca4ab861585fddc60d0
SerujanSatkunanathan <144580005+SerujanSatkunanathan@users.noreply.github.com>
2024-10-18 21:48:51 +0530

    Update country_info.php

--- Task 2/country_info.php
+++ Task 2/country_info.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"> <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"> 
     <title>Country Info</title>
     <style>
         body {
